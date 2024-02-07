554a56ada24e4296c5e4fdf497a7db2c2921597a
Michael Harris <mdillanh@gmail.com>
2024-02-07 17:36:25 -0600

    Added function to continue playing even when the first answer was wrong. Added option to press 'q' to quit

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,12 @@
+
 number = 10
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+guess = ''
+while guess != 'q' and guess != str(number):
+    guess = input("What number am I thinking of? ")
+    if guess == 'q':
+        print(f"The number was {number}.")
+    elif guess == str(number):
+        print("Congratulations! You guessed the right number.")
+    else:
+        print(f"Sorry! The number was {number}! Try again.")
